ec9e2a5ff201cc6cfbcbb39f7bc1c382cf8c3723
shinjiiiiiii <udauvergne@edenschool.fr>
2023-03-20 16:37:37 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,152 @@
+
+// Ex 1 
+// let str = ""
+// for(let i = 1; i<10 ; i++){
+//     str += i
+//     console.log(str);
+// }
+
+// Ex 2 
+// let str = ""
+// for(let i = 1; i<10 ; i++){
+//     str += i + " "
+//     console.log(str);
+// }
+
+// Ex3
+// let str = ""
+// for(let i = 1; i<100 ; i++){
+//     if(i % 2 ){
+//         str += i + " " 
+//     }
+//     console.log(str);
+// }
+
+// Ex 4 
+// let str = ""
+// for(let i = 1; i<100 ; i++){
+//     if(i %7 ){
+//     }else{
+//         str += i + " " 
+//         console.log(str);
+//     }
+// }
+
+// Ex 5 
+// let str = ""
+// for(let i = 1; i<10 ; i++){
+//     for(let a = 0; a<10; a++){
+//         str += i*a + " "
+//         console.log(str);
+//     }
+// }
+
+// Ex 6 
+// function n(e){
+//     let res = 0 
+//     for(let i = 1; i<=10; i++){
+//          res += i 
+//         console.log(res);
+//     }
+
+// } 
+// n()
+
+// Ex 7 
+// function n(e){
+//     let res = 1 
+//     for(let i = 1; i<10; i ++){
+//         res *=i 
+//         console.log(res);
+//     }
+// }
+// n()
+
+// Ex 8
+// function n(e){
+//     let res = 0 
+//     for(let i =10; i<30; i++){
+//         if(i %2){
+//         }else{
+//             res += i
+//             console.log(res);
+//         }
+//     }
+// }
+// n()
+
+// Ex 9
+
+// function n(e){
+//     let res = e * 9/5 + 32  
+//     console.log(res + "°F");
+// }
+// n(e)
+
+// Ex 10 
+// function sommeArr(e){
+//     let res = 0 
+//     for(let i = 0; i<e.length;i++){
+//         res += e[i]
+//         console.log(res);
+//     }
+// }
+// sommeArr([1,2,3,4,5,6,7,8,9,10])
+
+// Ex 11 
+
+// function moy(e){
+//     let res = 0 
+//     for(let i = 0; i<e.length; i++){
+//         res += e[i]/e.length
+//     }
+//     console.log(res);
+// }
+// moy([1,2,3,4,5,6,7,8,9,10])
+
+// Ex 12 
+
+// function intP(e){
+//     let arr = []
+//     for(let i = 0; i<e.length; i++){
+//         if(i > 0){
+            
+//         }else if(i === 0){
+//         } else {
+//             arr.push(i)
+//             console.log(arr);
+//         }
+//     }
+// }
+// intP([1,2,3,4,5,6,7,8,9,10])
+
+// Ex 13
+
+// function nMax(e){
+//     let res = e[0]
+//     for(let i = 0;i<e.length; i++){
+//         if(e[i]>res){
+//             res = e[i] 
+//             console.log(res);
+//         }
+//     }
+// }
+// nMax([1,2,3,4,5,6,7,8,9,10])
+
+// Ex 14 
+
+// Ex 15
+
+// function pos(e){
+//     let a = e.toString()
+//     let res = 0
+//     for(let i = 0; i<a.length;i ++){
+//         let int = parseInt(a[i])
+//         res += int 
+//         console.log(res);
+//     }
+// }
+// pos(111)
+
+// Ex 16 
+
